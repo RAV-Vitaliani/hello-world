ddaa043b3998bec6a385ac20ac62b59c9ae4753d
RAV-Vitaliani <38505530+RAV-Vitaliani@users.noreply.github.com>
2018-04-20 10:36:18 -0300

    Create ejemplo.java

---
+++ ejemplo.java
@@ -0,0 +1,12 @@
+package ejemplos;
+import java.util.Scanner;]]
+import javax.swing.JOptionPane;
+
+public class mainejemplo {
+
+	public static void main(String[] args) {
+	
+   String nombre= JOptionPane.showInputDialog("Ingrese su nombre: ");
+    JOptionPane.showMessageDialog(null, "Hola " + nombre);
+
+	}}
